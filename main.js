f075747f7538ac3df4a6436dd32dca3d77a3f300
Andretreccia <andreabtreccia@gmail.com>
2021-10-19 17:18:53 +0200

    Completamento primo esercizio

--- main.js
+++ main.js
@@ -1,37 +1,15 @@
 // chiedi all utente una parola
 let userWord = prompt("inserisci una parola per scoprire se è paindroma")
-/* console.log(userWord) */
-//verifica se la parola è palindrona
-        //per farlo inseriscila in un array e 
-//let wordArray = userWord.split("")
-//console.log(wordArray)
-        //clona l array
-//let wordArrayClone = [...wordArray]
-
-    //funzione
-//wordArrayClone.reverse()
-//console.log(wordArrayClone)
-//let userWordInvert = wordArrayClone.join("")
-//onsole.log(userWordInvert)
-
-//if (userWord == userWordInvert) {
-///console.log("palindroma")
-//}
-//else {
-//console.log("non  è palindroma")
-//}
-
+//con una funzione verifica che sia palinfroma
 function palindromaFunction(parola1) {
     let parola2 = parola1.split("").reverse().join("");
-    //console.log(parola2)
 
     if (parola1 == parola2)
-        console.log("palindromaaaaaaaaa")
+        alert("La parola è palindroma")
     else {
-        console.log("non è palondroma")
+        alert("La parola non è palondroma")
     }
 }
-
 let userWordreverse = palindromaFunction(userWord);
 
 
