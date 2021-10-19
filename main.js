882b0f587ac3a45b9dcd7cad124e6acf6b450ac4
Andretreccia <andreabtreccia@gmail.com>
2021-10-19 17:13:08 +0200

    Creata funzione per il primo esercizio

--- main.js
+++ main.js
@@ -3,22 +3,37 @@ let userWord = prompt("inserisci una parola per scoprire se è paindroma")
 /* console.log(userWord) */
 //verifica se la parola è palindrona
         //per farlo inseriscila in un array e 
-let wordArray = userWord.split("")
-console.log(wordArray)
+//let wordArray = userWord.split("")
+//console.log(wordArray)
         //clona l array
-let wordArrayClone = [...wordArray]
+//let wordArrayClone = [...wordArray]
 
     //funzione
-wordArrayClone.reverse()
-console.log(wordArrayClone)
-let userWordInvert = wordArrayClone.join("")
-console.log(userWordInvert)
+//wordArrayClone.reverse()
+//console.log(wordArrayClone)
+//let userWordInvert = wordArrayClone.join("")
+//onsole.log(userWordInvert)
 
-if (userWord == userWordInvert) {
-    console.log("palindroma")
+//if (userWord == userWordInvert) {
+///console.log("palindroma")
+//}
+//else {
+//console.log("non  è palindroma")
+//}
+
+function palindromaFunction(parola1) {
+    let parola2 = parola1.split("").reverse().join("");
+    //console.log(parola2)
+
+    if (parola1 == parola2)
+        console.log("palindromaaaaaaaaa")
+    else {
+        console.log("non è palondroma")
+    }
 }
-else {
-    console.log("non  è palindroma")
-}
-        //rovescia l ordine dell array clonato
-        //confronta i due array. 
+
+let userWordreverse = palindromaFunction(userWord);
+
+
+
+
