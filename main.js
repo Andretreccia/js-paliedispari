2f52021af1ac57fa42930de680226b5c149a5fe2
Andretreccia <andreabtreccia@gmail.com>
2021-10-19 16:13:21 +0200

    Creato codice funzionante

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 // chiedi all utente una parola
-
+let userWord = prompt("inserisci una parola per scoprire se è paindroma")
+/* console.log(userWord) */
 //verifica se la parola è palindrona
         //per farlo inseriscila in un array e 
+let wordArray = userWord.split("")
+console.log(wordArray)
         //clona l array
+let wordArrayClone = [...wordArray]
+
     //funzione
-        //dividila per la sua lunghezza.
+wordArrayClone.reverse()
+console.log(wordArrayClone)
+let userWordInvert = wordArrayClone.join("")
+console.log(userWordInvert)
+
+if (userWord == userWordInvert) {
+    console.log("palindroma")
+}
+else {
+    console.log("non  è palindroma")
+}
         //rovescia l ordine dell array clonato
         //confronta i due array. 
