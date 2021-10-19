fd628a80aa3128561bc703088e4036f16dcb0bbf
Andretreccia <andreabtreccia@gmail.com>
2021-10-19 15:21:43 +0200

    Dettati passaggi primo esercizo

---
+++ main.js
@@ -0,0 +1,9 @@
+// chiedi all utente una parola
+
+//verifica se la parola è palindrona
+        //per farlo inseriscila in un array e 
+        //clona l array
+    //funzione
+        //dividila per la sua lunghezza.
+        //rovescia l ordine dell array clonato
+        //confronta i due array. 
